7347e0bf73be02f9683c2c2a55c32fe44b980228
Due Ho & Natalie Blackburn <student@epicodus.com>
2014-03-06 13:37:02 -0800

    begins adding division functionality

--- lib/word_calculator.rb
+++ lib/word_calculator.rb
@@ -1,10 +1,17 @@
 def word_calculator(input)
-  number_array = input.scan(/\d/)
+  number_array = input.scan(/\d\.\d/)
   if input.include? "plus"
-    output = number_array[0].to_i + number_array[1].to_i 
+    output = number_array[0].to_f + number_array[1].to_f 
   elsif input.include? "minus"
-    output = number_array[0].to_i - number_array[1].to_i 
+    output = number_array[0].to_f - number_array[1].to_f 
+  elsif input.include? 'multiplied by' or input.include? 'times'
+    output = number_array[0].to_f * number_array[1].to_f
+  elsif input.include? "divided by"
+    puts number_array[0]
+    puts number_array[1]
+    output = number_array[0].to_f / number_array[1].to_f
   end
 end
 
 
+# \d{1,}
--- spec/word_calculator_spec.rb
+++ spec/word_calculator_spec.rb
@@ -8,4 +8,17 @@ describe("word_calculator") do
   it("subtracts 5 minus 3 and gets 2") do
     word_calculator("What is 5 minus 3?").should(eq(2))
   end
+  it("multiples 5 times 3 and gets 15") do
+    word_calculator("What is 5 times 3?").should(eq(15))
+  end
+  it("multiples 5 multiplied by 3 and gets 15") do
+    word_calculator("What is 5 multiplied by 3?").should(eq(15))
+  end
+  it("divides 15 by 3 and gets 5") do
+    word_calculator("What is 15 divided by 3?").should(eq(5))
+  end
+  it("divides 15.4 by 3.5 and gets 4.4") do
+    word_calculator("What is 15.4 divided by 3.5?").should(eq(4.4))
+  end
 end
+
